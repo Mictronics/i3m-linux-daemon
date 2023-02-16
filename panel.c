a0faf7b6cc52576dd9febde76283bd18dd8421cb
Mictronics <github@mictronics.de>
2023-02-16 19:11:08 +0100

    Include reference to I2C SMBUS functions.

--- panel.c
+++ panel.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <asm-generic/errno-base.h>
 #include <linux/i2c-dev.h>
+#include <i2c/smbus.h>
 
 #include "panel.h"
 #include "registers.h"
@@ -16,19 +17,18 @@
 #include "stats.h"
 #include "common.h"
 
-
-/* 
- * Panel descriptor. 
- * Panel descriptor is managed as a singleton object. 
+/*
+ * Panel descriptor.
+ * Panel descriptor is managed as a singleton object.
  */
-typedef struct {
+typedef struct
+{
 	bool is_initialized;
 	int i2c_desc;
 	unsigned int i2c_delay;
 } Panel;
 
 static Panel panel = {0};
-
 
 /*
  * Open i2c-connected device identified by {i2c-bus:addr} tuple.
@@ -41,14 +41,16 @@ static int __panel_open_i2c_device(int i2c_bus, int i2c_addr)
 	int err = 0;
 
 	i2c_devnum = open_i2c_dev(i2c_bus);
-	if (i2c_devnum < 0) {
+	if (i2c_devnum < 0)
+	{
 		sloge("i2c-%d: could not open: %d", i2c_bus, i2c_devnum);
 		err = i2c_devnum;
 		goto i2c_out_err0;
 	}
 
 	err = set_slave_addr(i2c_devnum, i2c_addr);
-	if (err < 0) {
+	if (err < 0)
+	{
 		sloge("i2c-%d:%02x could not set i2c slave: %d", i2c_bus, i2c_addr, err);
 		goto i2c_out_err1;
 	}
@@ -66,7 +68,8 @@ int panel_open_i2c(int i2c_bus, int i2c_addr, unsigned int i2c_delay)
 {
 	int i2c_devnum;
 
-	if ( panel.is_initialized ) {
+	if (panel.is_initialized)
+	{
 		sloge("panel i2c device is already open");
 		return -EEXIST;
 	}
@@ -83,7 +86,7 @@ int panel_open_i2c(int i2c_bus, int i2c_addr, unsigned int i2c_delay)
 
 void panel_close(void)
 {
-	if ( !panel.is_initialized )
+	if (!panel.is_initialized)
 		return;
 
 	close(panel.i2c_desc);
@@ -95,10 +98,12 @@ int panel_read_byte(unsigned regno)
 	int value;
 
 	value = i2c_smbus_read_byte_data(panel.i2c_desc, regno);
-	if (value < 0) {
+	if (value < 0)
+	{
 		sloge("Could not read register %02x: %d", regno, value);
 	}
-	else {
+	else
+	{
 		stat_inc_i2c_read_count();
 	}
 
@@ -116,16 +121,17 @@ int panel_write_byte(unsigned regno, int data)
 		usleep(panel.i2c_delay);
 
 	err = i2c_smbus_write_byte_data(panel.i2c_desc, regno, data);
-	if (err < 0) {
+	if (err < 0)
+	{
 		sloge("Could not write register %02x: %d", regno, err);
 	}
-	else {
+	else
+	{
 		stat_inc_i2c_write_count();
 	}
 
 	return err;
 }
-
 
 int panel_lookup_i2c_bus(void)
 {
@@ -136,13 +142,15 @@ int panel_lookup_i2c_bus(void)
 	int ret = -1;
 
 	adapters = gather_i2c_busses();
-	for (i = 0; adapters && adapters[i].name; ++i) {
+	for (i = 0; adapters && adapters[i].name; ++i)
+	{
 
 		fd = __panel_open_i2c_device(adapters[i].nr, I2C_PANEL_INTERFACE_ADDR);
 		if (fd < 0)
 			continue;
 
-		for (j = 0; j < 4; ++j) {
+		for (j = 0; j < 4; ++j)
+		{
 			signature[j] = i2c_smbus_read_byte_data(fd, (ATFP_REG_SIG0 + j));
 			if (signature[j] < 0)
 				break;
@@ -150,7 +158,8 @@ int panel_lookup_i2c_bus(void)
 		signature[j] = 0;
 		close(fd);
 
-		if (!strcmp("CLFP", signature)) {
+		if (!strcmp("CLFP", signature))
+		{
 			ret = adapters[i].nr;
 			goto lookup_out;
 		}
@@ -162,8 +171,8 @@ lookup_out:
 }
 
 /*
- * Return a bitmap of pending requests. 
- * In case of error: return 0 - meaning no requests. 
+ * Return a bitmap of pending requests.
+ * In case of error: return 0 - meaning no requests.
  */
 long panel_get_pending_requests(void)
 {
@@ -174,7 +183,7 @@ long panel_get_pending_requests(void)
 		return 0L;
 
 	/* no requests pending */
-	if ( !(value & 0x01) )
+	if (!(value & 0x01))
 		return 0L;
 
 	value = panel_read_byte(ATFP_REG_PENDR0);
@@ -193,7 +202,7 @@ int panel_set_temperature(int cpu_id, int temp)
 	/* assert((cpu_id >= 0) && (cpu_id <= 7)); */
 
 	err = panel_write_byte(temp_reg, temp);
-	if ( !err )
+	if (!err)
 		err = panel_write_byte(ATFP_REG_CPUTS, valid_mask);
 
 	return err;
@@ -208,7 +217,7 @@ int panel_set_frequency(int cpu_id, int freq)
 	/* assert((cpu_id >= 0) && (cpu_id <= 7)); */
 
 	err = panel_write_byte(freq_reg_lsb, (freq & 0xFF));
-	if ( !err )
+	if (!err)
 		err = panel_write_byte(freq_reg_msb, (0x80 | ((freq >> 8) & 0xFF)));
 
 	return err;
@@ -219,7 +228,7 @@ int panel_set_gpu_temp(int temp)
 	int err;
 
 	err = panel_write_byte(ATFP_REG_GPUT, temp);
-	if ( !err )
+	if (!err)
 		err = panel_write_byte(ATFP_REG_SENSORT, ATFP_MASK_SENSORT_GPUS);
 
 	return err;
@@ -231,13 +240,14 @@ int panel_set_hdd_temp(int hdd_id, int temp)
 	int temp_reg = ATFP_REG_HDD0T + hdd_id;
 	int valid_mask = 1 << hdd_id;
 
-	if ((hdd_id < 0) || (hdd_id > 7)) {
+	if ((hdd_id < 0) || (hdd_id > 7))
+	{
 		slogw("HDD: index out of range: %d", hdd_id);
 		return -EINVAL;
 	}
 
 	err = panel_write_byte(temp_reg, temp);
-	if ( !err )
+	if (!err)
 		err = panel_write_byte(ATFP_REG_HDDTS, valid_mask);
 
 	return err;
@@ -255,11 +265,11 @@ int panel_store_daemon_postcode(void)
 	int postcode_lsb;
 
 	err = panel_write_byte(ATFP_REG_POST_CODE_MSB, ATFP_DAEMON_POSTCODE_MSB);
-	if ( err )
+	if (err)
 		goto postcode_out;
 
 	err = panel_write_byte(ATFP_REG_POST_CODE_LSB, ATFP_DAEMON_POSTCODE_LSB);
-	if ( err )
+	if (err)
 		goto postcode_out;
 
 	/* selftest: read back */
@@ -271,4 +281,3 @@ int panel_store_daemon_postcode(void)
 postcode_out:
 	return err;
 }
-
